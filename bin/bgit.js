d1c01393908d3d51e2cca0eaa5e67d5a6de73698
crossjs <liwenfu@crossjs.com>
2015-02-22 10:11:51 +0800

    0.0.2

--- bin/bgit.js
+++ bin/bgit.js
@@ -2,19 +2,30 @@
 
 'use strict';
 
-var commander = require('commander');
+var commander = require('commander')
 
 commander
+  .description([
+    '',
+    '      __          _ __ ',
+    '     / /_  ____ _(_) /_',
+    '    / __ \\/ __ `/ / __/',
+    '   / /_/ / /_/ / / /_  ',
+    '  /_.___/\\__, /_/\\__/  ',
+    '        /____/         ',
+    '',
+    ''
+    ].join('\n'))
   .version(require('../package').version, '-v, --version')
-  .option('-s, --src <src>', 'Source root')
-  .option('-d, --dest <dest>', 'Building destination')
-  .option('-p, --port <port>', 'Listening port')
   .option('-q, --quiet', 'Not to open in browser')
-  .parse(process.argv);
+  .option('-s, --src <src>', 'Source root')
+  .option('-d, --dest <dest>', 'Build destination')
+  .option('-p, --port <port>', 'Listening port')
+  .parse(process.argv)
 
 require('../lib/index')({
+  quiet: commander.quiet,
   src: commander.src || 'src',
   dest: commander.dest || 'dist',
-  port: commander.port || 3000,
-  quiet: commander.quiet
-});
+  port: commander.port || 3000
+})
